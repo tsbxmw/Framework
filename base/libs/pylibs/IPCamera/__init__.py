e5f8e44c8fd4bd7e0f4a3a6f3bdb03d7837deee7
tsbxmw <1050636648@qq.com>
2017-11-09 14:16:51 +0800

    [FILE ADD] add __init__.py to pylibs

---
+++ base/libs/pylibs/IPCamera/__init__.py
@@ -0,0 +1,7 @@
+# _*_ coding: utf-8 _*_
+
+"""
+define HIKVision
+"""
+
+from .HIKVision import getVideoLog
---
+++ base/libs/pylibs/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+
+"""
+define File IPCamera Log NetWork
+"""
+
+__version__ == "2.7.2"
+
+from .File import *
+from .IPCamera import *
+from .Log import *
+from .NetWork import *
