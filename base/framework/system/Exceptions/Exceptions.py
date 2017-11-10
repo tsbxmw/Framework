9a0c45d18ad21c447f45a008439beb9269c4b8aa
tsbxmw <1050636648@qq.com>
2017-11-10 10:27:23 +0800

    [FILE ADD] add system.json & Exceptions.py

---
+++ base/framework/system/Exceptions/Exceptions.py
@@ -0,0 +1,27 @@
+# _*_ coding: utf-8 _*_
+
+"""
+    define Exceptions
+"""
+import os, time, json, sys
+
+sys.path.append("Log")
+from Log.LogShow import LogShow
+
+class Exceptions(object):
+    def __init__(self):
+        self.func_name = "Exceptions"
+        self.ls = LogShow(self.func_name)
+
+    def show(self,data):
+        self.ls.log_print("Exceptions", str(data))
+
+    def exception(self,exception):
+        if exception:
+            pass
+        else:
+            pass
+
+
+    
+        
