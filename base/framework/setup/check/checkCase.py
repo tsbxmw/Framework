e8279908df0e65ea41e9a70e8ad0c9e82b398543
wei.meng <1050636648@qq.com>
2018-01-16 11:45:53 +0800

    [M] check case add config read

--- base/framework/setup/check/checkCase.py
+++ base/framework/setup/check/checkCase.py
@@ -1,6 +1,6 @@
 # _*_ coding = utf-8 _*_
 
-import os, time, sys
+import os, time, sys, json
 
 
 class CheckCase(object):
@@ -9,6 +9,7 @@ class CheckCase(object):
         sys.path.append(os.path.abspath(os.path.join(localpath,"..\\..\\system")))
         from framework_tool import Framework_Tool as FT
         ft = FT()
+        self.case_config_path = sys.path.append(os.path.abspath(os.path.join(localpath,"..\\..\\..\\config\\json\\cases.json")))
         self.func_name = "CheckCase"
         self.ls = ft.getLog(self.func_name)
         
@@ -23,12 +24,28 @@ class CheckCase(object):
         else:
             return False
 
-    
-     
     def checkCase(self):
         func_name_ = "checkTool"
         self.ls.log_print("system", func_name_ + " start now...")
+        
         if True:          
              return True
         else :
             return False
+            
+    def readCaseConfig(self):
+        func_name_ = "readCaseConfig"
+        self.ls.log_print("system", func_name_ + " start ")
+        try:
+            if os.path.exists(self.case_config_path):
+                self.ls.log_print("system", func_name_ + " find the case config" )
+            else:
+                self.ls.log_print("error", func_name_ + " cannot find the case config ")
+                return False
+                
+            f = open(self.case_config_path, 'r')           
+            for file_line in json.load(f)["all_cases_name"]:
+                self.ls.log_print("debug", file_line)
+            f.close()
+        except Exception,e:
+            self.ls.log_print("error", func_name_ + " error " + str(e))
